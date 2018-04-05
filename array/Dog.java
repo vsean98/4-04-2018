170145d59e9faf45a005555a43290426bf8a7fa9
Cameron Hyman <cameronhyman@Camerons-MacBook-Air.local>
2018-04-05 13:34:02 -0400

    first commit

---
+++ array/Dog.java
@@ -0,0 +1,24 @@
+package array;
+
+public class Dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		String[]animals= {"Bird","Cat","dog","Rat"};
+	String temp=animals[0];
+	
+	animals[0]=animals[2];
+	animals[2]=temp;
+	
+	
+	
+	
+	
+	
+	}
+
+}
---
+++ array/Largest.java
@@ -0,0 +1,10 @@
+package array;
+
+public class Largest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
